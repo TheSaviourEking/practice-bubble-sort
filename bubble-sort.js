0b5235c08023a3e8a8111ecb9e9044a80495d2d4
Saviour Eking <saviour.eking@outlook.com>
2023-10-28 00:46:08 +0100

    SOLVED: fix bubble-sort.js
    Computed performance in google docs, and it has a verified time complexity of O(n^2) which is manageable for small datasets, but gets really terrible the larger datasets .

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,28 +1,48 @@
+/*********************************************************************************************
+ * WHILE LOOP IMPLEMENTATION
+function bubbleSort(arr){
+	let swap = true;
+	while(swap){
+		swap = false;
+		for (let i = 0; i < arr.length; i++){
+			if (arr[i] > arr[i+1]){ 
+                        [arr[i], arr[i+1]]=[arr[i+1], arr[i]];
+                        swap = true;
+                }
+
+		}
+	}
+	return arr;
+}
+console.log(bubbleSort([3, 5, 1, 7, 2, 4, 10, 0]));
+***********************************************************************************************/
+
 function bubbleSort(arr) {
-  let swapped = true;
+  let swapped = false;
 
-  while (swapped) {
-    swapped = false;
-    // Iterate through the array
-    for (let i = 0; i < arr.length; i++) {
-      // If the current value is greater than its neighbor to the right
+  // Iterate through the array
+  for (let i = 0; i < arr.length; i++) {
 
-      if (arr[i] > arr[i + 1]) {
-        // Swap those values
-        [arr[i + 1], arr[i]] = [arr[i], arr[i + 1]];
-        swapped = true;
+    // If the current value is greater than its neighbor to the right
+    if (arr[i] > arr[i + 1]) {
+      // Swap those values
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+      swapped = true;
 
-        // Do not move this console.log
-        console.log(arr.join(","));
-      }
+
+      // Do not move this console.log
+      console.log(arr.join(","));
     }
   }
 
   // If you get to the end of the array and no swaps have occurred, return
+  if (!swapped) {
+    return arr;
+  }
 
   // Otherwise, repeat from the beginning
-  return arr;
+  return bubbleSort(arr);
+
 }
-// arr = [2, 4, 6, 8, 1, 3, 5, 7, 9]
-// console.log(bubbleSort(arr));
+
 module.exports = bubbleSort;
