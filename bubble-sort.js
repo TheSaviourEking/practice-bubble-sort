5fe3d6aab5d55a35b16e29abbfa55f28118ac321
Saviour Eking <saviour.eking@outlook.com>
2023-10-27 03:03:12 +0000

    SOLVED: fix bubble sort algorithm
    Computed performance in google docs and time complexity is O(n^2)

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,28 @@
-
 function bubbleSort(arr) {
+  let swapped = true;
 
+  while (swapped) {
+    swapped = false;
     // Iterate through the array
-
+    for (let i = 0; i < arr.length; i++) {
       // If the current value is greater than its neighbor to the right
+
+      if (arr[i] > arr[i + 1]) {
         // Swap those values
+        [arr[i + 1], arr[i]] = [arr[i], arr[i + 1]];
+        swapped = true;
 
         // Do not move this console.log
         console.log(arr.join(","));
+      }
+    }
+  }
 
-    // If you get to the end of the array and no swaps have occurred, return
+  // If you get to the end of the array and no swaps have occurred, return
 
-    // Otherwise, repeat from the beginning
-
+  // Otherwise, repeat from the beginning
+  return arr;
 }
-
+arr = [2, 4, 6, 8, 1, 3, 5, 7, 9]
+console.log(bubbleSort(arr));
 module.exports = bubbleSort;
